0e73b0e4108dfce004358d7a66092648c1cf7c45
VojinPopovic <vojinpopovic3@gmail.com>
2023-06-21 19:56:47 +0200

    updated modal

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -11,49 +11,47 @@ export default function CreatePost({ setRenderPost }) {
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        class="w-[60vw] aspect-square fixed mx-auto my-auto left-0 right-0 top-0 bottom-0 py-6 px-6 flex flex-col justify-center"
+        className="w-[60vw] max-w-[600px] aspect-square fixed mx-auto my-auto left-0 right-0 top-0 bottom-0 py-6 px-6 flex flex-col justify-center"
       >
-        <div class="relative py-3">
-          <div class="absolute inset-0 bg-gradient-to-r from-orange-100 to-orange-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-          <div class="relative px-6 py-10 bg-white shadow-lg sm:rounded-3xl">
-            <div class="max-w-md mx-auto">
+        <div className="relative py-3">
+          <div className="absolute inset-0 bg-gradient-to-r from-orange-100 to-orange-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+          <div className="relative px-6 py-10 bg-white shadow-lg sm:rounded-3xl">
+            <div className="max-w-md mx-auto">
               <div>
-                <h1 class="text-2xl font-semibold">Make a new post</h1>
+                <h1 className="text-2xl font-semibold">Make a new post</h1>
               </div>
-              <div class="divide-y divide-gray-200">
-                <div class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                  <div class="relative">
+              <div className="divide-y divide-gray-200">
+                <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+                  <div className="relative">
                     <input
-                      autocomplete="off"
                       name="title"
                       type="text"
-                      class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
                       placeholder="Email address"
                     />
                     <label
-                      for="title"
-                      class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                      htmlFor="title"
+                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
                     >
                       Title
                     </label>
                   </div>
-                  <div class="relative">
+                  <div className="relative">
                     <input
-                      autocomplete="off"
                       name="content"
                       type="password"
-                      class="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                      className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
                       placeholder="Password"
                     />
                     <label
-                      for="content"
-                      class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                      htmlFor="content"
+                      className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
                     >
                       Content
                     </label>
                   </div>
-                  <div class="relative">
-                    <button class="_accent-color-bg text-white rounded-md px-2 py-1">
+                  <div className="relative">
+                    <button className="_accent-color-bg text-white rounded-md px-2 py-1">
                       Submit
                     </button>
                   </div>
