e8b894013729acc8e2d6c57f0a28c0075a0fc044
VojinPopovic <vojinpopovic3@gmail.com>
2023-07-14 18:38:55 +0200

    landing page style changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <div className="relative w-full h-[70px] sm:h-[120px] _text-color flex items-center px-[3%] z-10">
-      <Link href="/">
-        <Image src={Logo} height={50} width={50} alt=""></Image>
-      </Link>
-      <div className="absolute w-1/2 max-w-[300px] flex justify-between mx-auto left-0 right-0 font-semibold">
-        <Link href="/about">About</Link>
-        <Link href="/new">New</Link>
-        <Link href="/feedback">Feedback</Link>
+    <div className="relative mx-[3%]">
+      <div className="relative w-full max-w-[900px] h-[71px] sm:h-[120px] _text-color flex items-center justify-between z-10 mx-auto">
+        <Link href="/">
+          <Image src={Logo} height={50} width={50} alt=""></Image>
+        </Link>
+        <div className="w-1/2 max-w-[300px] justify-between font-semibold hidden md:flex">
+          <Link href="/about">About</Link>
+          <Link href="/new">New</Link>
+          <Link href="/feedback">Feedback</Link>
+        </div>
       </div>
     </div>
   );
